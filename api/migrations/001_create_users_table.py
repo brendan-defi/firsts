598e9bffcea9b9d436ab540f2011dd5c4c4f8495
Brendan Forster <bpforster24+products@gmail.com>
2024-02-23 13:13:20 -0800

    Updated postgres setup and implemented users table

--- api/migrations/001_create_users_table.py
+++ api/migrations/001_create_users_table.py
@@ -5,7 +5,12 @@ steps = [
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY NOT NULL,
             email VARCHAR(100) NOT NULL,
-            password VARCHAR(100) NOT NULL
+            password VARCHAR(100) NOT NULL,
+            firstname VARCHAR(100),
+            lastname VARCHAR(100),
+            created_at DATE NOT NULL,
+            updated_at DATE NOT NULL,
+            deleted_at DATE
         );
         """,
         # "Down" SQL statement
