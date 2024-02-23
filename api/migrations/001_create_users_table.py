5acd1816fcd2519d7295897d18196b3c087bbc36
Brendan Forster <bpforster24+products@gmail.com>
2024-02-23 15:18:16 -0800

    Implemented database uniqueness constraint on usernames

--- api/migrations/001_create_users_table.py
+++ api/migrations/001_create_users_table.py
@@ -4,7 +4,7 @@ steps = [
         """
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY NOT NULL,
-            username VARCHAR(100) NOT NULL,
+            username VARCHAR(100) NOT NULL UNIQUE,
             password VARCHAR(100) NOT NULL,
             firstname VARCHAR(100),
             lastname VARCHAR(100),
