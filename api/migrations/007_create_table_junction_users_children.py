409ddc12d0ad0a5017c5747fcf343386ae6f26e1
Brendan Forster <bpforster24+products@gmail.com>
2024-02-25 16:27:02 -0800

    Added uniqueness constraint on primary user<>child relationships

--- api/migrations/007_create_table_junction_users_children.py
+++ api/migrations/007_create_table_junction_users_children.py
@@ -6,6 +6,7 @@ steps = [
             user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
             child_id INTEGER NOT NULL REFERENCES children(id) ON DELETE CASCADE,
             relationship_type_id INTEGER NOT NULL REFERENCES relationship_types(id) ON DELETE RESTRICT,
+            is_primary_relationship BOOLEAN NOT NULL DEFAULT FALSE,
             created_at TIMESTAMPTZ NOT NULL,
             updated_at TIMESTAMPTZ NOT NULL,
             deleted_at TIMESTAMPTZ,
@@ -15,6 +16,18 @@ steps = [
         # "Down" SQL statement
         """
         DROP TABLE IF EXISTS juction_users_children;
+        """,
+    ],
+    [
+        # "Up" SQL statement
+        """
+        CREATE UNIQUE INDEX unique_user_child_primary_relationship
+        ON junction_users_children (child_id)
+        WHERE is_primary_relationship = TRUE;
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE IF EXISTS unique_user_child_primary_relationship;
         """,
     ]
 ]
