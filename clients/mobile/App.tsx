0f3ebdc0316fabd9873d7dd84d25dc15fcfc3217
Brendan Forster <bpforster24+products@gmail.com>
2024-02-26 15:06:03 -0800

    Redid mobile scaffolding to use typescript

--- clients/mobile/App.tsx
+++ clients/mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, Neo!</Text>
+      <Text>Hello, Neo.</Text>
       <StatusBar style="auto" />
     </View>
   );
--- clients/mobile/README.md
+++
@@ -1,5 +0,0 @@
-# React Native Client using Expo
-
-To get started:
-1. Navigate to the `firsts/clients/mobile` directory in terminal
-2. Run the command `npx expo start`
