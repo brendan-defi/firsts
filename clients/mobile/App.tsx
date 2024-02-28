cc6d367f88a9e0e9fd69f8eb44771b130cb1c534
Brendan Forster <bpforster24+products@gmail.com>
2024-02-27 16:06:35 -0800

    Updated App and config files

--- clients/mobile/App.tsx
+++ clients/mobile/App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Homescreen from "./screens/Homescreen";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, Neo.</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    component={Homescreen}
+                    name="Home"
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen
+                    component={Login}
+                    name="Login"
+                    options={{ title: "Login" }}
+                />
+                <Stack.Screen
+                    component={Signup}
+                    name="Signup"
+                    options={{ title: "Signup" }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
