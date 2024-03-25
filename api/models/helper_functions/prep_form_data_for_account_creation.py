f3d461eb257b33a7ebeb522aef7ee220b2046e94
Brendan Forster <bpforster24+products@gmail.com>
2024-03-25 14:34:56 -0700

    renamed and updated a helper function

--- api/models/helper_functions/prep_form_data_for_account_creation.py
+++ api/models/helper_functions/prep_form_data_for_account_creation.py
@@ -1,14 +1,15 @@
-from models.users import UserFormForAccountCreation, UserDataForAccountCreation
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+from authenticator import authenticator
+from models.users import UserFormForAccountCreation, UserDataForAccountCreation
 
-def data_to_user_data_for_account_creation(
+def prep_form_data_for_account_creation(
         data: UserFormForAccountCreation
 ):
     return UserDataForAccountCreation(
         username=data.username,
-        hashed_password=data.generate_hashed_password(),
+        hashed_password=authenticator.hash_password(data.password),
         created_at=datetime.now(tz=ZoneInfo("GMT")),
         updated_at=datetime.now(tz=ZoneInfo("GMT"))
     )
