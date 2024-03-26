366c90fd18ea00543ca0e67b2563e280d6ea3677
Brendan Forster <bpforster24+products@gmail.com>
2024-03-26 14:50:24 -0700

    implemented form submission for user signup

--- clients/mobile/screens/Signup/SignupPassword.tsx
+++ clients/mobile/screens/Signup/SignupPassword.tsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Image,
     Text,
     SafeAreaView,
     View,
     TextInput,
+    TouchableOpacity,
     Pressable,
 } from "react-native";
 
 import { useSignupContext } from "../../contexts/signupContext";
-import SignupNavigationButton from "../../components/Buttons/SignupNavigationButton";
+// import SignupNavigationButton from "../../components/Buttons/SignupNavigationButton";
 import { SignupProps } from "../../types/signupProps";
 import { navigationButtonStyles } from "../../styles/navigationButton";
 import { signupStyles } from "../../styles/signup";
 
 export default function SignupPassword({ navigation }: SignupProps) {
     const { signupInfo, setSignupInfo } = useSignupContext();
+    const [error, setError] = useState("");
+
+    const handleSignupFormSubmission = async () => {
+        if (signupInfo.password !== signupInfo.passwordConfirmation) {
+            setError("Password and Password Confirmation do not match.")
+            return;
+        }
+        const signupUrl = `http://localhost:8000/api/users`
+        const fetchConfig = {
+            method: "POST",
+            body: JSON.stringify(signupInfo),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+        const response = await fetch(signupUrl, fetchConfig)
+        if (!response.ok) {
+            const error = await response.json()
+            setError(error.detail)
+        }
+        return response.status;
+    }
 
     return (
         <SafeAreaView style={signupStyles.container}>
@@ -29,12 +52,13 @@ export default function SignupPassword({ navigation }: SignupProps) {
                 <View style={signupStyles.inputContainer}>
                     <Text style={signupStyles.formHeader}>Password</Text>
                     <TextInput
-                        onChangeText={(text) =>
+                        onChangeText={(text) => {
+                            setError("");
                             setSignupInfo({
                                 ...signupInfo,
                                 password: text,
-                            })
-                        }
+                            });
+                        }}
                         value={signupInfo.password}
                         placeholder="P@ssW0rd!"
                         autoCapitalize="none"
@@ -45,12 +69,13 @@ export default function SignupPassword({ navigation }: SignupProps) {
                 <View style={signupStyles.inputContainer}>
                     <Text style={signupStyles.formHeader}>Confirm Password</Text>
                     <TextInput
-                        onChangeText={(text) =>
+                        onChangeText={(text) => {
+                            setError("");
                             setSignupInfo({
                                 ...signupInfo,
                                 passwordConfirmation: text,
-                            })
-                        }
+                            });
+                        }}
                         value={signupInfo.passwordConfirmation}
                         placeholder="P@ssW0rd!"
                         autoCapitalize="none"
@@ -58,16 +83,16 @@ export default function SignupPassword({ navigation }: SignupProps) {
                         style={signupStyles.formField}
                     />
                 </View>
+                {error && <Text>{error}</Text>}
             </View>
 
             <View style={signupStyles.ctaContainer}>
-                <SignupNavigationButton
-                    navigation={navigation}
-                    destination="SignupPassword"
-                    text="Continue"
-                    buttonStyle={navigationButtonStyles.primaryCta}
-                    textStyle={navigationButtonStyles.primaryCtaText}
-                />
+                <TouchableOpacity
+                    onPress={handleSignupFormSubmission}
+                    style={navigationButtonStyles.primaryCta}
+                >
+                    <Text style={navigationButtonStyles.primaryCtaText}>Register</Text>
+                </TouchableOpacity>
                 <Pressable
                     style={signupStyles.secondaryCta}
                     onPress={() => navigation.navigate("Login")}
