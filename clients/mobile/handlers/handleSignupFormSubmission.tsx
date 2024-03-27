508b966d01d6848ca43ed85d1365a920b301e499
Brendan Forster <bpforster24+products@gmail.com>
2024-03-26 20:47:32 -0700

    implemented login component of signup handler

--- clients/mobile/handlers/handleSignupFormSubmission.tsx
+++ clients/mobile/handlers/handleSignupFormSubmission.tsx
@@ -4,22 +4,49 @@ export default async function handleSignupFormSubmission(
     signupFormData: SignupInfo,
     errorSetter: React.Dispatch<React.SetStateAction<string>>
 ) {
+    // FORM VALIDATION
     if (signupFormData.password !== signupFormData.passwordConfirmation) {
-        errorSetter("Password inputs do not match.")
+        errorSetter("Password inputs do not match.");
         return;
     }
-    const signupUrl = `http://localhost:8000/api/users`
-    const fetchConfig = {
+
+    // SIGNUP
+    const signupUrl = `http://localhost:8000/api/users`;
+    const signupConfig = {
         method: "POST",
         body: JSON.stringify(signupFormData),
         headers: {
             "Content-Type": "application/json",
         },
+    };
+    const signupResponse = await fetch(signupUrl, signupConfig);
+    if (!signupResponse.ok) {
+        const signupError = await signupResponse.json();
+        errorSetter(String(signupError.detail));
+        return signupResponse.status;
     }
-    const response = await fetch(signupUrl, fetchConfig)
-    if (!response.ok) {
-        const error = await response.json()
-        errorSetter(error.detail)
+
+    // LOGIN
+    const loginCreds = new FormData();
+    loginCreds.append("username", signupFormData.username);
+    loginCreds.append("password", signupFormData.password);
+    const loginUrl = `http://localhost:8000/token`;
+    const loginConfig = {
+        method: "POST",
+        body: loginCreds,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+    };
+    const loginResponse = await fetch(loginUrl, loginConfig);
+    if (!loginResponse.ok) {
+        const loginError = await loginResponse.json();
+        errorSetter(String(loginError.detail));
+        return loginResponse.status;
     }
-    return response.status;
+    const loginData = await loginResponse.json();
+
+    // TOKEN STORAGE
+    // TODO
+
 }
