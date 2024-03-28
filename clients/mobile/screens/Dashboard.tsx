ef946400977739ca61d30502a04c087a3f64a89f
Brendan Forster <bpforster24+products@gmail.com>
2024-03-27 21:09:27 -0700

    implemented simple logout button

--- clients/mobile/screens/Dashboard.tsx
+++ clients/mobile/screens/Dashboard.tsx
@@ -1,10 +1,18 @@
 // import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { useAuthContext } from "../contexts/authContext";
 
 export default function Dashboard() {
+    const {deleteBearerToken} = useAuthContext()
     return (
         <View style={styles.container}>
             <Text>Dashboard</Text>
+            <TouchableOpacity
+                onPress={deleteBearerToken}
+            >
+                <Text>Logout</Text>
+            </TouchableOpacity>
         </View>
     );
 }
