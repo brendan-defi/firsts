97b7e5a47c041650c9ad04de7aa63587bb83e98c
Brendan Forster <bpforster24+products@gmail.com>
2024-04-02 13:25:15 -0700

    moved logout link to settings screen

--- clients/mobile/screens/Home/Dashboard.tsx
+++ clients/mobile/screens/Home/Dashboard.tsx
@@ -1,13 +1,10 @@
 // import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import LogoutLink from "../../components/LogoutLink";
-
 export default function Dashboard() {
     return (
         <View style={styles.container}>
             <Text>Dashboard</Text>
-            <LogoutLink />
         </View>
     );
 }
--- clients/mobile/screens/Home/Settings.tsx
+++ clients/mobile/screens/Home/Settings.tsx
@@ -1,10 +1,13 @@
 // import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import LogoutLink from "../../components/LogoutLink";
+
 export default function Settings() {
     return (
         <View style={styles.container}>
             <Text>Settings</Text>
+            <LogoutLink />
         </View>
     );
 }
