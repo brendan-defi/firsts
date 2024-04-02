9f34895777a26ca4a9c9dfebc3e86a2cbda15486
Brendan Forster <bpforster24+products@gmail.com>
2024-04-02 13:22:47 -0700

    created screens index file for easier importation

---
+++ clients/mobile/screens/index.ts
@@ -0,0 +1,10 @@
+export {default as SplashScreen} from "./Auth/SplashScreen";
+export {default as Login} from "./Auth/Login";
+export {default as SignupUsername} from "./Auth/Signup/SignupUsername";
+export {default as SignupPassword} from "./Auth/Signup/SignupPassword";
+
+export {default as Dashboard} from "./Home/Dashboard";
+export {default as Calendar} from "./Home/Calendar";
+export {default as CreateMemory} from "./Home/CreateMemory";
+export {default as List} from "./Home/List";
+export {default as Settings} from "./Home/Settings";
