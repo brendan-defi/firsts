d350e9e7d8d7e5089c101ae215724569a94c9563
Brendan Forster <bpforster24+products@gmail.com>
2024-04-23 13:13:50 -0700

    removed comments

--- clients/mobile/navigators/signupNavigator.tsx
+++ clients/mobile/navigators/signupNavigator.tsx
@@ -6,10 +6,6 @@ import { SignupContextProvider } from "../contexts/signupContext";
 import {
     SignupUsername,
     SignupPassword,
-    // NUXName,
-    // NUXCreateChild,
-    // NUXConnectChild,
-    // NUXWelcome,
 } from "../screens";
 
 const SignupStack = createNativeStackNavigator<SignupStackParamList>();
@@ -31,22 +27,6 @@ export default function SignupNavigator() {
                     name="SignupPassword"
                     component={SignupPassword}
                 />
-                {/* <SignupStack.Screen
-                    name="NUXName"
-                    component={NUXName}
-                />
-                <SignupStack.Screen
-                    name="NUXCreateChild"
-                    component={NUXCreateChild}
-                />
-                <SignupStack.Screen
-                    name="NUXConnectChild"
-                    component={NUXConnectChild}
-                />
-                <SignupStack.Screen
-                    name="NUXWelcome"
-                    component={NUXWelcome}
-                /> */}
             </SignupStack.Navigator>
         </SignupContextProvider>
     );
