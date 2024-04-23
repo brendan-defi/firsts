5378df122f7aaadbdcf910bb4692f90615a6d4f5
Brendan Forster <bpforster24+products@gmail.com>
2024-04-23 15:38:11 -0700

    updates to NUXName and settings screens

--- clients/mobile/screens/Auth/Signup/NUXName.tsx
+++ clients/mobile/screens/Auth/Signup/NUXName.tsx
@@ -1,12 +1,139 @@
-import { Text, SafeAreaView, View } from "react-native";
-import LogoutLink from "../../../components/LogoutLink";
+import React, { useEffect, useState } from "react";
+import {
+    Image,
+    Text,
+    SafeAreaView,
+    View,
+    TextInput,
+    Pressable,
+    TouchableOpacity,
+} from "react-native";
 
-export default function NUXName() {
+import { NuxProps } from "../../../types/nuxProps";
+import { UserDataForAccountUpdate } from "../../../types/userDataForAccountUpdate";
+
+import { authStyles } from "../../../styles/authentication";
+import { navigationButtonStyles } from "../../../styles/navigationButton";
+
+import useToken from "../../../hooks/useToken";
+import getUserData from "../../../utils/getUserData";
+import updateUserData from "../../../utils/updateUserData";
+import { useAuthContext } from "../../../contexts/authContext";
+
+export default function NUXName({ navigation }: NuxProps) {
+    const bearerToken = useToken();
+    const { setHasCompletedNux } = useAuthContext();
+    const [userDataForUpdateForm, setUserDataForUpdateForm] =
+        useState<UserDataForAccountUpdate>({
+            username: null,
+            firstname: undefined,
+            lastname: undefined,
+            completed_nux: null,
+        });
+
+    useEffect(() => {
+        const setCurrentUserData = async () => {
+            const currentUserData = await getUserData(bearerToken);
+            if (currentUserData) {
+                setUserDataForUpdateForm({
+                    username: currentUserData.username,
+                    firstname: currentUserData.firstname,
+                    lastname: currentUserData.lastname,
+                    completed_nux: currentUserData.completed_nux,
+                });
+            }
+        };
+        setCurrentUserData();
+    }, [bearerToken]);
+
+    const handleFormInput = (text: string, key: string) => {
+        setUserDataForUpdateForm({
+            ...userDataForUpdateForm,
+            [key]: text,
+        });
+    };
+    const handleUserDataUpdate = async () => {
+        const updatedUserData = await updateUserData(
+            userDataForUpdateForm,
+            bearerToken
+        );
+        if (!updatedUserData) {
+            console.error("some sort of error")
+        }
+        navigation.navigate("NUXCreateChild");
+    };
+    const handleSkipOnboarding = async () => {
+        const skipNuxFormData: UserDataForAccountUpdate = {
+            ...userDataForUpdateForm,
+            completed_nux: true,
+        }
+        const updatedUserData = await updateUserData(
+            skipNuxFormData,
+            bearerToken
+        );
+        if (!updatedUserData) {
+            console.error("some sort of error")
+        }
+        setHasCompletedNux(true);
+    };
+
+    console.log(userDataForUpdateForm)
+
     return (
-        <SafeAreaView>
-            <View>
-                <Text>NUXName</Text>
-                <LogoutLink />
+        <SafeAreaView style={authStyles.container}>
+            <View style={authStyles.titleContainer}>
+                <Image
+                    style={authStyles.title}
+                    source={require("../../../assets/homescreen/title.png")}
+                />
+                <Text style={authStyles.formHeader}>
+                    Welcome to Firsts! To get started, tell us more about
+                    yourself and your family.
+                </Text>
+            </View>
+            <View style={authStyles.formContainer}>
+                <View style={authStyles.inputContainer}>
+                    <Text style={authStyles.formHeader}>Your First Name</Text>
+                    <TextInput
+                        onChangeText={(text) =>
+                            handleFormInput(text, "firstname")
+                        }
+                        value={userDataForUpdateForm.firstname}
+                        placeholder="Snuggie"
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        style={authStyles.formField}
+                    />
+                </View>
+                <View style={authStyles.inputContainer}>
+                    <Text style={authStyles.formHeader}>Your Last Name</Text>
+                    <TextInput
+                        onChangeText={(text) =>
+                            handleFormInput(text, "lastname")
+                        }
+                        value={userDataForUpdateForm.lastname}
+                        placeholder="Wuggie"
+                        autoCapitalize="none"
+                        autoCorrect={false}
+                        style={authStyles.formField}
+                    />
+                </View>
+            </View>
+            <View style={authStyles.ctaContainer}>
+                <TouchableOpacity
+                    onPress={handleUserDataUpdate}
+                    style={navigationButtonStyles.primaryCta}
+                >
+                    <Text style={navigationButtonStyles.primaryCtaText}>
+                        Continue
+                    </Text>
+                </TouchableOpacity>
+                <Pressable
+                    style={authStyles.secondaryCta}
+                    onPress={handleSkipOnboarding}
+                >
+                    <Text>Skip Onboarding</Text>
+                </Pressable>
             </View>
         </SafeAreaView>
     );
