d290a788020fd37f6038160e5c85b2cd6b1b0717
Brendan Forster <bpforster24+products@gmail.com>
2024-04-23 13:08:10 -0700

    Added logic into Main to accommodate NUX

--- clients/mobile/Main.tsx
+++ clients/mobile/Main.tsx
@@ -3,14 +3,20 @@ import { NavigationContainer } from "@react-navigation/native";
 import { useAuthContext } from "./contexts/authContext";
 
 import AuthNavigator from "./navigators/authNavigator";
+import NuxNavigator from "./navigators/nuxNavigator";
 import AppNavigator from "./navigators/homeNavigator";
 
 export default function Main() {
-    const { isLoggedIn } = useAuthContext();
+    const { isLoggedIn, hasCompletedNux } = useAuthContext();
 
     return (
         <NavigationContainer>
-            {isLoggedIn ? <AppNavigator /> : <AuthNavigator />}
+            {!isLoggedIn
+                ? <AuthNavigator />
+                : !hasCompletedNux
+                    ? <NuxNavigator />
+                    : <AppNavigator />
+            }
         </NavigationContainer>
     );
 }
